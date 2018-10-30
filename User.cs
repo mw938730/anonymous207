75a4a6f6e4a4ca035e371f882f05566345ac5c0c
Mark <mcwarren1@buffs.wtamu.edu>
2018-10-30 00:37:53 -0500

    Update User.cs
    added buyer and seller inheritance

--- User.cs
+++ User.cs
@@ -5,11 +5,8 @@ using system;
 			string userName{get;set;}
 			string passWord{get;set;}
 			string email{get;set}
-			int creditCard{get;set}
 			string address{get;set}
 			string zip{get;set}
-			string billingAddress{get;set}
-			string billingZipCode{get;set}
 			int bidID{get;set;}
 			int userID{get;set;)
 			
@@ -26,4 +23,54 @@ using system;
 				//insert method/
 			}//end editAccountInfo
 		}//end User
+	public class Buyer:User
+	{
+		int creditCard
+		string billingAddress
+		string billingZipCode
+		public Buyer(string userName, string address, string passWord, string email, int zip, int bidID
+					, int userID,int credCard ,  string billAddress, string billZip ):base(userName,  address,  passWord,  email,  zip,  bidID
+					,  userID){
+        setCreditCard(credCard);
+        setBillingAddress(billAddress);
+		setBillingZip(billZip);
+    }//end Buyer
+	public void setCreditCard(int card){
+        creditCard = card ;
+    }
+    public float getCreditCard(){
+        return creditCard;
+    }
+    public void setBillingAddress(string address){
+        billAddress = address ;
+    }
+    public float getBillingAddress(){
+        return billAddress;
+    }
+    public void setBillingZip(string zip){
+        billZip = zip ;
+    }
+    public float getBillingZip(){
+        return billZip;
+    }
+}//MIGHT NOT NEED THIS BRACKET
+	}//end class buyer	
+		
+	public class Seller:User
+	{
+		//insert any additional attributes for seller class here!
+		public Buyer(string userName, string passWord, string email, int zip, int bidID
+					, int userID):base(userName,  passWord,  email,  zip,  bidID
+					,  userID){
+
+    }//end Seller
+
+    }
+}//MIGHT NOT NEED THIS BRACKET
+	}//end class buyer			
+		
+		
+		
+		
+		
 	}//end namespace
